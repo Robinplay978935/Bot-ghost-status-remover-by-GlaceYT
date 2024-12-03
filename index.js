e93b3e1892fd1b572235fdfd24249c1c8097bd6e
Robinplay978935 <Midshop10@gmail.com>
2024-12-03 03:19:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["👑 MidShop MainSystem", "🌐 https://MidShop.mysellauth.com/"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
